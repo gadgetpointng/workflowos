09ee11ef54e7f66a8bd3e6a43c64f87212c898e2
gadgetpointng <gadgetpoint.ng@gmail.com>
2026-08-16 01:53:50 +0200

    Use supported manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,6 +11,6 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#08111f',
     theme_color: '#08111f',
     orientation: 'portrait-primary',
-    icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any maskable' }]
+    icons: [{ src: '/icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'maskable' }]
   };
 }
